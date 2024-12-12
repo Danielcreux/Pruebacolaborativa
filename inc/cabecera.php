fa24d802790c35bca4d7ff4bc876ffbb1c404729
Danielcreux <jd2000_fs@hotmail.com>
2024-12-12 19:20:54 +0100

    3

---
+++ inc/cabecera.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <main>
+            <h2>Este es el archivo de Joshué</h2>
+            
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
---
+++ inc/piedepagina.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <main>
+            
+            
+        </main>
+        <footer>
+            <h5>Esto es el texto del final</h5>
+            
+        </footer>
+    </body>
+</html>
