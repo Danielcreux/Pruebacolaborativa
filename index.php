571cb03fb47b5e650c77450dbda321338222cb97
Danielcreux <jd2000_fs@hotmail.com>
2024-12-12 18:34:40 +0100

    meta2

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Ejercicio de prueba de github</title>
         <meta charset="utf-8">
+        <meta type="descripcion" content="Esta es una descripcion">
     </head>
     <body>
         <h1>Yo soy Joshué Freire en archivo principal</h1>
