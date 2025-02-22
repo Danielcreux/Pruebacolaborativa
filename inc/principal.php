58daaf327aef38c5eda40518189dde5f3d3844fe
Danielcreux <jd2000_fs@hotmail.com>
2025-02-22 20:22:18 +0100

    prueba

---
+++ inc/principal.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <header>
+            <h1>Esto es algo nuevos</h1>
+            
+        </header>
+        <main>
+          
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
